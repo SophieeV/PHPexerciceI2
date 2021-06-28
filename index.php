19d48a0afd433ec56a7344fb94e104248d92907a
SophieeV <sophievesin@wanadoo.fr>
2021-06-28 14:27:59 +0200

    ajout fichiers

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice I2 </title>
+  </head>
+  <body>
+  <?php
+  $date = new DateTime('2021-06-23');
+  echo $date->format('Y-m-d');
+  ?>
+  </body>
+</html>
